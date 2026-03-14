58bfa8d5edc4d9958768f825ffefb9b5ea8fd8cc
EyssDev <eyss1159@gmail.com>
2026-03-14 12:18:12 +0100

    add menu

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Mojito",
+    description: "Menthe fraîche, citron vert, eau gazeuse",
+    price: 12,
+    image: "https://via.placeholder.com/300x200"
+  },
+  {
+    id: 2,
+    name: "Piña Colada",
+    description: "Ananas, coco, crème",
+    price: 14,
+    image: "https://via.placeholder.com/300x200"
+  },
+  {
+    id: 3,
+    name: "Cocktail Detox",
+    description: "Concombre, citron, menthe",
+    price: 10,
+    image: "https://via.placeholder.com/300x200"
+  }
+];
+
+export default products;
